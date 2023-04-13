4641ff7abcce32b1e458bda8c1064b59b26b49d9
cwen13 <cwenrich13@gmail.com>
2023-04-13 17:45:14 -0400

    set up starter files

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./../styles/Header.css"
+
+// Need to have About Me, Portfolio, Contact, and Resume
+export Header = () => {
+  return (
+    <nav>
+      <ol>
+	<li class="" >About Me </li>
+	<li class="" >Portfolio </li>
+	<li class="" >Contact </li>
+	<li class="" >Resume </li>
+      </ol>
+    </nav>
+  );
+}
