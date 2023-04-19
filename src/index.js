af7ebb6789c6731d0662233743c49e27f59ed268
cwen13 <cwenrich13@gmail.com>
2023-04-19 17:32:45 -0400

    Get email service to work

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "semantic-ui-css/semantic.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
