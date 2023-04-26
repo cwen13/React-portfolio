8c671bed741cba9d33a228fd543843dceeb92a10
cwen13 <cwenrich13@gmail.com>
2023-04-25 22:18:05 -0400

    update about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,15 +11,18 @@ const AboutMe =() => {
 	style={{borderRadius: "50%",
 		margin: "2rem"}}
       />
-      <h2>About Me</h2>
+      <h2>About Me</h2>      
       <p>
-	I am seeking a position as a web developer for a great company
+	I am seeking a position as a web developer for a great company taht will foster my developement as a developer.
       </p>
       <p>
-	I am mechnical Engineer by degree but always had a fasication with programming. In my studies I would use MatLab and it gave me such a sense of acomplishment of completeing a challenge. This drive to program led me to self stufy of Python that resulted in a few web scrapping jobs and an oppotunity to write a document search tool for a previous employer.
+	Having gone to school for mechanical engineering I was always interested in programming. I would think of ways to solve class problems with a program. So in the short time we used matlab I was excited and loved it. But I was too deep in my studies and I thought about changing my major but just finished my degree.
       </p>
       <p>
-	since then I received my Engineering Intern certifate and completed a edX code camp thorught Case WEstern Reserve University. This camp was thrilling and truely feels like I am in the right field of engineering.
+	This drive to develop code compelled me to learn how to scrape websites with python and even got a couple of gigs doing it even. In addition, being able to contribute code to the work place helped keep the fire alive in me.
+      </p>
+      <p>
+	So then while not feeling I was in the right place for me I sought out coding to make the career change. Having done the code camp through CWRU I am nothing but thrilled to see out a job I will not only be capable of doing but will enjoy for a very long time.
       </p>
     </div>
   );
